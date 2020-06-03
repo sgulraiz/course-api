68978ff39cc73e671ef5c56af802a1411466ce5d
sgulraiz <sgulraiz@gmail.com>
2020-06-02 21:18:42 -0400

    first commit

---
+++ src/test/java/com/ivisionsys/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivisionsys.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
